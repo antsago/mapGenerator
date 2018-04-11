2d8cc7fdf0942b4267f3e90d3c4597cd8691da10
Antonio <sago.ant@gmail.com>
2018-04-11 23:04:36 +0200

    refactor: reordered terrainControls

--- terraincontrols.js
+++ terraincontrols.js
@@ -18,6 +18,109 @@ function meshDraw() {
     visualizePoints(meshSVG, meshDual ? meshVxs : meshPts);
 }
 
+
+var primDiv = d3.select("div#prim");
+var primSVG = addSVG(primDiv);
+
+var primH = zero(generateGoodMesh(4096));
+
+function primDraw() {
+    visualizeVoronoi(primSVG, primH, -1, 1);
+    drawPaths(primSVG, 'coast', contour(primH, 0));
+}
+
+primDraw()
+
+var erodeDiv = d3.select("div#erode");
+var erodeSVG = addSVG(erodeDiv);
+
+function generateUneroded() {
+    var mesh = generateGoodMesh(4096);
+    var h = add(slope(mesh, randomVector(4)),
+                cone(mesh, runif(-1, 1)),
+                mountains(mesh, 50));
+    h = peaky(h);
+    h = fillSinks(h);
+    h = setSeaLevel(h, 0.5);
+    return h;
+}
+
+var erodeH = primH;
+var erodeViewErosion = false;
+
+function erodeDraw() {
+    if (erodeViewErosion) {
+        visualizeVoronoi(erodeSVG, erosionRate(erodeH));
+    } else {
+        visualizeVoronoi(erodeSVG, erodeH, 0, 1);
+    }
+    drawPaths(erodeSVG, "coast", contour(erodeH, 0));
+}
+
+var physDiv = d3.select("div#phys");
+var physSVG = addSVG(physDiv);
+var physH = erodeH;
+
+var physViewCoast = false;
+var physViewRivers = false;
+var physViewSlope = false;
+var physViewHeight = true;
+
+function physDraw() {
+    if (physViewHeight) {
+        visualizeVoronoi(physSVG, physH, 0);
+    } else {
+        physSVG.selectAll("path.field").remove();
+    }
+    if (physViewCoast) {
+        drawPaths(physSVG, "coast", contour(physH, 0));
+    } else {
+        drawPaths(physSVG, "coast", []);
+    }
+    if (physViewRivers) {
+        drawPaths(physSVG, "river", getRivers(physH, 0.01));
+    } else {
+        drawPaths(physSVG, "river", []);
+    }
+    if (physViewSlope) {
+        visualizeSlopes(physSVG, {h:physH});
+    } else {
+        visualizeSlopes(physSVG, {h:zero(physH.mesh)});
+    }
+}
+
+var cityDiv = d3.select("div#city");
+var citySVG = addSVG(cityDiv);
+
+var cityViewScore = true;
+
+function newCityRender(h) {
+    h = h || generateCoast({npts:4096, extent: defaultExtent});
+    return {
+        params: defaultParams,
+        h: h,
+        cities: []
+    };
+}
+var cityRender = newCityRender(physH);
+function cityDraw() {
+    cityRender.terr = getTerritories(cityRender);
+    if (cityViewScore) {
+        var score = cityScore(cityRender.h, cityRender.cities);
+        visualizeVoronoi(citySVG, score, d3.max(score) - 0.5);
+    } else {
+        visualizeVoronoi(citySVG, cityRender.terr);
+    }
+    drawPaths(citySVG, 'coast', contour(cityRender.h, 0));
+    drawPaths(citySVG, 'river', getRivers(cityRender.h, 0.01));
+    drawPaths(citySVG, 'border', getBorders(cityRender));
+    visualizeSlopes(citySVG, cityRender);
+    visualizeCities(citySVG, cityRender);
+}
+
+var finalDiv = d3.select("div#final");
+var finalSVG = addSVG(finalDiv);
+
 function generatePointsButton()
 {
     meshDual = false;
@@ -38,18 +141,6 @@ function voronoiCornersButton()
     meshDual = !meshDual;
     meshDraw();
 }
-
-var primDiv = d3.select("div#prim");
-var primSVG = addSVG(primDiv);
-
-var primH = zero(generateGoodMesh(4096));
-
-function primDraw() {
-    visualizeVoronoi(primSVG, primH, -1, 1);
-    drawPaths(primSVG, 'coast', contour(primH, 0));
-}
-
-primDraw()
 
 function resetFlatButton() 
 {
@@ -105,32 +196,6 @@ function setSeaLevelButton()
     primDraw();
 }
 
-var erodeDiv = d3.select("div#erode");
-var erodeSVG = addSVG(erodeDiv);
-
-function generateUneroded() {
-    var mesh = generateGoodMesh(4096);
-    var h = add(slope(mesh, randomVector(4)),
-                cone(mesh, runif(-1, 1)),
-                mountains(mesh, 50));
-    h = peaky(h);
-    h = fillSinks(h);
-    h = setSeaLevel(h, 0.5);
-    return h;
-}
-
-var erodeH = primH;
-var erodeViewErosion = false;
-
-function erodeDraw() {
-    if (erodeViewErosion) {
-        visualizeVoronoi(erodeSVG, erosionRate(erodeH));
-    } else {
-        visualizeVoronoi(erodeSVG, erodeH, 0, 1);
-    }
-    drawPaths(erodeSVG, "coast", contour(erodeH, 0));
-}
-
 function generateUnerodedButton()
 {
     erodeH = generateUneroded();
@@ -166,38 +231,6 @@ function showErosionButton()
 {
     erodeViewErosion = !erodeViewErosion;
     erodeDraw();
-}
-
-var physDiv = d3.select("div#phys");
-var physSVG = addSVG(physDiv);
-var physH = erodeH;
-
-var physViewCoast = false;
-var physViewRivers = false;
-var physViewSlope = false;
-var physViewHeight = true;
-
-function physDraw() {
-    if (physViewHeight) {
-        visualizeVoronoi(physSVG, physH, 0);
-    } else {
-        physSVG.selectAll("path.field").remove();
-    }
-    if (physViewCoast) {
-        drawPaths(physSVG, "coast", contour(physH, 0));
-    } else {
-        drawPaths(physSVG, "coast", []);
-    }
-    if (physViewRivers) {
-        drawPaths(physSVG, "river", getRivers(physH, 0.01));
-    } else {
-        drawPaths(physSVG, "river", []);
-    }
-    if (physViewSlope) {
-        visualizeSlopes(physSVG, {h:physH});
-    } else {
-        visualizeSlopes(physSVG, {h:zero(physH.mesh)});
-    }
 }
 
 function generateCoastButton()
@@ -236,35 +269,6 @@ function hideHeightButton()
     physDraw();
 }
 
-var cityDiv = d3.select("div#city");
-var citySVG = addSVG(cityDiv);
-
-var cityViewScore = true;
-
-function newCityRender(h) {
-    h = h || generateCoast({npts:4096, extent: defaultExtent});
-    return {
-        params: defaultParams,
-        h: h,
-        cities: []
-    };
-}
-var cityRender = newCityRender(physH);
-function cityDraw() {
-    cityRender.terr = getTerritories(cityRender);
-    if (cityViewScore) {
-        var score = cityScore(cityRender.h, cityRender.cities);
-        visualizeVoronoi(citySVG, score, d3.max(score) - 0.5);
-    } else {
-        visualizeVoronoi(citySVG, cityRender.terr);
-    }
-    drawPaths(citySVG, 'coast', contour(cityRender.h, 0));
-    drawPaths(citySVG, 'river', getRivers(cityRender.h, 0.01));
-    drawPaths(citySVG, 'border', getBorders(cityRender));
-    visualizeSlopes(citySVG, cityRender);
-    visualizeCities(citySVG, cityRender);
-}
-
 function renderCityButton()
 {
     cityRender = newCityRender();
@@ -288,9 +292,6 @@ function showTerritoriesButton()
     cityViewScore = !cityViewScore;
     cityDraw();
 }
-
-var finalDiv = d3.select("div#final");
-var finalSVG = addSVG(finalDiv);
 
 function drawMapButton()
 {
